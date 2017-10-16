0461bcb1959275ae38a7f55e72d385355019357a
Aaron McRuer <a.mcruer@live.com>
2017-10-16 18:29:17 -0500

    adding model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+# https://gis.stackexchange.com/questions/73322/geodjango-using-lat-lon-to-buffer-from-point
+class TaggingPoint(models.Model):
+    objects = models.GeoManager()
+    point = models.PointField(srid=4326)
+
+    def latitude(self):
+        return self.point.y
+
+    def longitude(self):
+        return self.point.x
