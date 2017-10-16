0c4e96e14456eb1c8f92f168a193bc090aaa4a25
Aaron McRuer <a.mcruer@live.com>
2017-10-16 18:56:12 -0500

    serializer set up.

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,11 +1,10 @@
-from rest_framework import serializers
 from backend.models import TaggingPoint
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 
-
 class TaggingPointSerializer(GeoFeatureModelSerializer):
-    model = TaggingPoint
-    geo_field = "point"
 
-    fields = ('id', 'address', 'city', 'state')
+    class Meta:
+        model = TaggingPoint
+        geo_field = "point"
+        fields = ('id', 'address', 'city', 'state')
