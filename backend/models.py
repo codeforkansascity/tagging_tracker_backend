2fa26903d3f66c0c38731cc06820a0df10b90896
Aaron McRuer <a.mcruer@live.com>
2017-12-13 13:05:16 -0600

    small cleanup on Address model (remove dupe field) and making the formatting PEP-8 kosher

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from django.db import models as base_models
 from django.contrib.gis.db import models as gis_models
 
+
 class Address(gis_models.Model):
     objects = gis_models.GeoManager()
     point = gis_models.PointField(srid=4326)
@@ -17,7 +18,6 @@ class Address(gis_models.Model):
     tenant_name = gis_models.CharField(max_length=12, blank=True)
     tenant_phone = gis_models.CharField(max_length=12, blank=True)
     tenant_email = gis_models.CharField(max_length=12, blank=True)
-    owner_email = gis_models.CharField(max_length=12, blank=True)
     follow_up_owner_needed = gis_models.BooleanField(default=False)
     land_bank_property = gis_models.BooleanField(default=False)
     date_updated = gis_models.DateField(auto_now=True)
@@ -28,18 +28,19 @@ class Address(gis_models.Model):
     def longitude(self):
         return self.point.x
 
+
 class Tag(base_models.Model):
-  address = base_models.ForeignKey('Address')
-  creator_user_id = base_models.CharField(max_length=255)
-  last_updated_user_id = base_models.CharField(max_length=255)
-  crossed_out = base_models.BooleanField(default=False)
-  date_updated = base_models.DateField(auto_now=True)
-  description = base_models.CharField(max_length=255)
-  gang_related = base_models.BooleanField(default=False)
-  img = base_models.CharField(max_length=1000, blank=True)
-  neighborhood = base_models.CharField(max_length=255, blank=True)
-  racially_motivated = base_models.BooleanField(default=False)
-  square_footage = base_models.CharField(max_length=50, blank=True)
-  surface = base_models.CharField(max_length=100, blank=True)
-  tag_words = base_models.CharField(max_length=255, blank=True)
-  tag_initials = base_models.CharField(max_length=20, blank=True)
+    address = base_models.ForeignKey('Address')
+    creator_user_id = base_models.CharField(max_length=255)
+    last_updated_user_id = base_models.CharField(max_length=255)
+    crossed_out = base_models.BooleanField(default=False)
+    date_updated = base_models.DateField(auto_now=True)
+    description = base_models.CharField(max_length=255)
+    gang_related = base_models.BooleanField(default=False)
+    img = base_models.CharField(max_length=1000, blank=True)
+    neighborhood = base_models.CharField(max_length=255, blank=True)
+    racially_motivated = base_models.BooleanField(default=False)
+    square_footage = base_models.CharField(max_length=50, blank=True)
+    surface = base_models.CharField(max_length=100, blank=True)
+    tag_words = base_models.CharField(max_length=255, blank=True)
+    tag_initials = base_models.CharField(max_length=20, blank=True)
