47db177f54b7921afe9047110dbef6a0a7f01539
Aaron McRuer <a.mcruer@live.com>
2018-01-13 14:05:26 -0600

    added .csv exporter, added .csv url endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^address/$', views.address_list),
     url(r'^address/(?P<pk>[0-9]+)/$', views.address_detail),
     url(r'^tags/$', views.tag_list),
-    url(r'^tags/(?P<pk>[0-9]+)/$', views.tag_detail)
+    url(r'^tags/(?P<pk>[0-9]+)/$', views.tag_detail),
+    url(r'^address/address.csv', views.csv_address_export),
 ]
