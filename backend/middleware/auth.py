1601cf1691111a808a9145a4f5b11dfaeefb8245
Aaron McRuer <a.mcruer@live.com>
2018-10-06 17:00:58 -0500

    Now using DRF status codes.

--- backend/middleware/auth.py
+++ backend/middleware/auth.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse
 from backend.controllers.controller_utils import HTTP_STATUS_OK, HTTP_STATUS_UNAUTHORIZED, HTTP_STATUS_FORBIDDEN,\
     HTTP_STATUS_INTERNAL_SERVER_ERROR
 
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN, HTTP_500_INTERNAL_SERVER_ERROR
+
 
 class AuthMiddleware:
     def __init__(self, get_response):
@@ -16,7 +18,7 @@ class AuthMiddleware:
             auth = request.META.get("HTTP_AUTHORIZATION", None)
 
             if not auth or len(auth.split()) != 2:
-                return JsonResponse({"Error": "Unauthenticated"}, status=HTTP_STATUS_UNAUTHORIZED)
+                return JsonResponse({"Error": "Unauthenticated"}, status=HTTP_401_UNAUTHORIZED)
 
             parts = auth.split()
             token = parts[1]
@@ -31,17 +33,17 @@ class AuthMiddleware:
                 }
             )
 
-            if not auth_response.status == HTTP_STATUS_OK:
-                if auth_response.status == HTTP_STATUS_FORBIDDEN or auth_response.status == HTTP_STATUS_UNAUTHORIZED:
-                    return JsonResponse({"Error": "Unauthorized"}, status=HTTP_STATUS_UNAUTHORIZED)
+            if not auth_response.status == HTTP_200_OK:
+                if auth_response.status == HTTP_403_FORBIDDEN or auth_response.status == HTTP_401_UNAUTHORIZED:
+                    return JsonResponse({"Error": "Unauthorized"}, status=HTTP_401_UNAUTHORIZED)
                 else:
-                    return JsonResponse({"Error": "Internal Server Error"}, status=HTTP_STATUS_INTERNAL_SERVER_ERROR)
+                    return JsonResponse({"Error": "Internal Server Error"}, status=HTTP_500_INTERNAL_SERVER_ERROR)
 
             if request.method == "POST" or request.method == "PUT":
                 authenticated_data = json.loads(auth_response.content.decode('utf-8'))
                 request_data = json.loads(request.body.decode('utf-8'))
 
                 if not authenticated_data['id'] == request_data['creator_user_id']:
-                    return JsonResponse({'Error': 'Unauthorized to create tags'}, status=HTTP_STATUS_FORBIDDEN)
+                    return JsonResponse({'Error': 'Unauthorized to create tags'}, status=HTTP_403_FORBIDDEN)
 
         return self.get_response(request)
