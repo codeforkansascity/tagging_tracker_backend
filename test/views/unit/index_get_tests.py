249eabd2b229272aa91330bebb5daa32602016bb
Adam Mertz <adammertz@gmail.com>
2019-01-12 09:30:42 -0600

    Revert back to Django response class as DRF response breaks app

--- test/views/unit/index_get_tests.py
+++ test/views/unit/index_get_tests.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import status
 
 from backend.controllers.index import index
@@ -6,5 +7,5 @@ from backend.controllers.index import index
 def test_index_returns_status():
     request = {}
     response = index(request)
-    assert response.data == {"status": "running"}
+    assert json.loads(response.content) == {"status": "running"}
     assert response.status_code == status.HTTP_200_OK
