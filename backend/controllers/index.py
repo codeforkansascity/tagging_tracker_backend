9ad389be80042f479d7af27b7c2bf26375a783bc
Adam Mertz <adammertz@gmail.com>
2019-01-12 09:30:42 -0600

    Revert back to Django response class as DRF response breaks app

--- backend/controllers/index.py
+++ backend/controllers/index.py
@@ -1,9 +1,8 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from rest_framework import status
-from rest_framework.response import Response
 
 
 @csrf_exempt
 def index(request):
-    return Response({"status": "running"}, status=status.HTTP_200_OK)
+    return JsonResponse({"status": "running"}, status=status.HTTP_200_OK)
