26340d3e599c40aacf3b65fc41f7112bae0837d4
Adam Mertz <adammertz@gmail.com>
2019-01-13 12:13:48 -0600

    Log deletion of address

--- backend/views/address.py
+++ backend/views/address.py
@@ -1,9 +1,13 @@
+import logging
 from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from backend.models import Address
 from backend.serializers import AddressSerializer
+
+
+logger = logging.getLogger(__name__)
 
 
 class AddressView(APIView):
@@ -20,5 +24,11 @@ class AddressView(APIView):
 
     def delete(self, request, pk):
         address = self.get_object(pk)
+        data = {
+            "street": address.street,
+            "city": address.city,
+            "zip": address.zip
+        }
         address.delete()
+        logger.debug(f"{data['street']} {data['city']}, {data['zip']} deleted")
         return Response()
