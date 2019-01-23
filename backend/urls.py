b726a223271a73292d0601f36702c992c431f661
Adam Mertz <adammertz@gmail.com>
2019-01-23 17:11:29 -0600

    test create_address

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^index/?$', index.index),
     url(r'^address/(?P<pk>[0-9]+)/tags/?$', address.address_tags),
     url(r'^address/(?P<pk>[0-9]+)/?$', AddressView.as_view(), name="address"),
-    url(r'^address/?$', address.address_list),
+    url(r'^address/?$', address.address_list, name="address-list"),
     url(r'^address/address.csv', csv.csv_address_export),
     url(r'^tags/?$', tag.tag_list, name="tag_list"),
     url(r'^tags/(?P<pk>[0-9]+)/?$', tag.tag_detail, name="tag"),
--- test/views/integration/address_tests.py
+++ test/views/integration/address_tests.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from django.contrib.gis.geos import Point
 from django.urls import reverse
@@ -58,3 +60,29 @@ def test_delete_address(client, fake):
     response = client.delete(reverse("address", kwargs={"pk": address.id}))
     assert response.status_code == status.HTTP_200_OK
     assert Address.objects.filter(pk=address.id).exists() is False
+
+
+def test_create_address(client, fake):
+    data = {
+        "neighborhood": "Some neighborhood",
+        "street": fake.street_address(),
+        "city": fake.city(),
+        "state": fake.state(),
+        "zip": fake.zipcode(),
+        "owner_name": fake.name(),
+        "owner_email": fake.safe_email(),
+        "tenant_name": fake.name(),
+        "tenant_email": fake.safe_email(),
+        "creator_user_id": "some id",
+        "last_updated_user_id": "some other id",
+        "point": "POINT(1 1)"
+    }
+
+    response = client.post(reverse("address-list"), data=json.dumps(data), content_type="application/json")
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    response_data = json.loads(response.content)
+    observed_data = response_data["properties"]
+    assert observed_data["neighborhood"] == data["neighborhood"]
+    assert observed_data["street"] == data["street"]
