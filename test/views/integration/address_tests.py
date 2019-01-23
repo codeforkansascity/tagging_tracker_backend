dab817236108496137ea6b8a9f1fe9c333886fcf
Adam Mertz <adammertz@gmail.com>
2019-01-23 17:30:56 -0600

    list address tests

--- test/views/integration/address_tests.py
+++ test/views/integration/address_tests.py
@@ -135,3 +135,53 @@ def test_get_address_tags(client, fake):
 
     response_data = json.loads(response.content)
     assert [tag_one.id, tag_two.id] == sorted([t["id"] for t in response_data])
+
+
+def test_list_addresses(client, fake):
+    address_one = Address(
+        point=Point(1, 2),
+        neighborhood="Some neighborhood",
+        street=fake.street_address(),
+        city=fake.city(),
+        state=fake.state(),
+        zip=fake.zipcode(),
+        creator_user_id="some id",
+        last_updated_user_id="some id",
+        owner_name=fake.first_name(),
+        owner_email=fake.safe_email(),
+        tenant_name=fake.name(),
+        tenant_email=fake.safe_email(),
+        follow_up_owner_needed=True,
+        land_bank_property=True,
+        type_of_property=1
+    )
+    address_one.save()
+    address_one.refresh_from_db()
+
+    address_two = Address(
+        point=Point(1, 2),
+        neighborhood="Some neighborhood",
+        street=fake.street_address(),
+        city=fake.city(),
+        state=fake.state(),
+        zip=fake.zipcode(),
+        creator_user_id="some id",
+        last_updated_user_id="some id",
+        owner_name=fake.first_name(),
+        owner_email=fake.safe_email(),
+        tenant_name=fake.name(),
+        tenant_email=fake.safe_email(),
+        follow_up_owner_needed=True,
+        land_bank_property=True,
+        type_of_property=1
+    )
+    address_two.save()
+    address_two.refresh_from_db()
+
+    response = client.get(reverse("address-list"))
+
+    assert response.status_code == status.HTTP_200_OK
+
+    response_data = json.loads(response.content)
+    observed_data = response_data["features"]
+    assert [address_one.id, address_two.id] == sorted([a["id"] for a in observed_data])
