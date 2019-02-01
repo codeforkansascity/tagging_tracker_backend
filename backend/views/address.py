7f13e1a7abbda6c818982598a9dab948c9064711
Adam Mertz <Amertz08@users.noreply.github.com>
2019-01-31 19:42:56 -0600

    Issue 89: Refactored address_list as class view (#91)
    This PR refactors the address_list function as a class based view

--- backend/views/address.py
+++ backend/views/address.py
@@ -1,5 +1,7 @@
 import logging
 from django.http import Http404
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -32,3 +34,23 @@ class AddressView(APIView):
         address.delete()
         logger.debug(f"{data['street']} {data['city']}, {data['zip']} deleted")
         return Response()
+
+
+class AddressListView(APIView):
+
+    parser_classes = (JSONParser,)
+
+    def get(self, request):
+        addresses = Address.objects.all()
+        serializer = AddressSerializer(addresses, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AddressSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
