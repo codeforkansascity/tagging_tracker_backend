a28c690caa80d00884a7de321bf5f6282b9331c1
Adam Mertz <adammertz@gmail.com>
2019-02-04 19:14:40 -0600

    Got rid of redundant url

--- backend/urls.py
+++ backend/urls.py
@@ -5,8 +5,7 @@ from backend.views.address import AddressView, AddressListView, AddressTagsView
 from backend.views.csv import TagDownloadView, AddressDownloadView
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'^index/?$', index),
+    url(r'^$', index, name="index"),
     url(r'^address/(?P<pk>[0-9]+)/tags/?$', AddressTagsView.as_view(), name="address-tags"),
     url(r'^address/(?P<pk>[0-9]+)/?$', AddressView.as_view(), name="address"),
     url(r'^address/?$', AddressListView().as_view(), name="address-list"),
