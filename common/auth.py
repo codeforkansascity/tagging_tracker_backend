182c361463afaaf98427fc76ec657aaca12420a6
Adam Mertz <adammertz@gmail.com>
2019-02-06 23:04:49 -0600

    Removed requires_scope decorator

--- common/auth.py
+++ common/auth.py
@@ -1,15 +1,11 @@
 import logging
 import os
-from functools import wraps
 
 import jwt
 from django.conf import settings
 from django.contrib.auth import authenticate
-from rest_framework import status
-from rest_framework.response import Response
 
-# This entire module was taken from https://bit.ly/2t8LZVD
-from rest_framework.views import APIView
+# This module was base off of https://bit.ly/2t8LZVD
 
 
 logger = logging.getLogger(__name__)
@@ -42,26 +38,3 @@ def has_valid_scope(request, required_scope):
         token_scopes = decoded["scope"].split()
         return required_scope in token_scopes
     return False
-
-
-def requires_scope(required_scope):
-    """Determines if the required scope is present in the access token
-    Args:
-        required_scope (str): The scope required to access the resource
-    """
-    def require_scope(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            if settings.DISABLE_AUTH:
-                logger.debug("auth disabled requires_scope bypassed")
-                return f(*args, **kwargs)
-
-            request = args[1] if isinstance(args[0], APIView) else args[0]
-            if has_valid_scope(request, required_scope):
-                return f(*args, **kwargs)
-
-            return Response(
-                {"message": "You do not have access to this resource"}, status=status.HTTP_403_FORBIDDEN
-            )
-        return decorated
-    return require_scope
--- test/common/auth_unit_tests.py
+++ test/common/auth_unit_tests.py
@@ -2,7 +2,7 @@ import pytest
 from rest_framework import status
 from rest_framework.views import APIView
 
-from common.auth import jwt_get_username_from_payload_handler, get_token_auth_header, requires_scope, has_valid_scope
+from common.auth import jwt_get_username_from_payload_handler, get_token_auth_header, has_valid_scope
 
 pytestmark = pytest.mark.unit
 
@@ -76,77 +76,3 @@ def test_has_valid_scope_returns_false_if_scope_not_found(mocker, request_builde
     }
 
     assert has_valid_scope(request, scope) is False
-
-
-@pytest.mark.usefixtures("enable_auth")
-def test_requires_scope_valid_scope_returns_function_call(mocker, request_builder):
-    scope = "read:write"
-    payload = {
-        "scope": scope
-    }
-
-    jwt_decode = mocker.patch("common.auth.jwt.decode")
-    jwt_decode.return_value = payload
-
-    @requires_scope(scope)
-    def view_func(*args, **kwargs):
-        return "Hello"
-
-    request = request_builder("GET", "/some/endpoint")
-    request.META = {
-        "HTTP_AUTHORIZATION": "Bearer token"
-    }
-
-    response = view_func(request)
-    assert response == "Hello"
-    jwt_decode.assert_called_once()
-
-
-@pytest.mark.usefixtures("enable_auth")
-def test_requires_scope_valid_scope_returns_class_method_call(mocker, request_builder):
-    scope = "read:write"
-    payload = {
-        "scope": scope
-    }
-
-    jwt_decode = mocker.patch("common.auth.jwt.decode")
-    jwt_decode.return_value = payload
-
-    class MyView(APIView):
-
-        @requires_scope(scope)
-        def get(self, request):
-            return "Hello"
-
-    request = request_builder("GET", "/some/endpoint")
-    request.META = {
-        "HTTP_AUTHORIZATION": "Bearer token"
-    }
-
-    response = MyView().get(request)
-    assert response == "Hello"
-    jwt_decode.assert_called_once()
-
-
-@pytest.mark.usefixtures("enable_auth")
-def test_requires_scope_invalid_scope_returns_403(mocker, request_builder):
-    scope = "read:write"
-    payload = {
-        "scope": scope
-    }
-
-    jwt_decode = mocker.patch("common.auth.jwt.decode")
-    jwt_decode.return_value = payload
-
-    @requires_scope("hello")
-    def view_func(*args, **kwargs):
-        return "Hello"
-
-    request = request_builder("GET", "/some/endpoint")
-    request.META = {
-        "HTTP_AUTHORIZATION": "Bearer token"
-    }
-
-    response = view_func(request)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert response.data == {"message": "You do not have access to this resource"}
