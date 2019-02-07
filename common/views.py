e4aa89218fc852f836810ab72efbbd01eed6b801
Adam Mertz <adammertz@gmail.com>
2019-02-06 22:42:19 -0600

    Created BaseView to handle endpoint authentication

--- common/views.py
+++ common/views.py
@@ -1,7 +1,28 @@
+import logging
+
+from django.conf import settings
 from djqscsv import render_to_csv_response
 from rest_framework.views import APIView
 
-from common.auth import requires_scope
+from common.auth import requires_scope, has_valid_scope
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseView(APIView):
+    scopes = {}
+
+    def check_permissions(self, request):
+        if settings.DISABLE_AUTH:
+            logger.debug("auth disabled scope check skipped")
+            return True
+
+        scope = self.scopes.get(request.method.lower())
+        if scope is None:
+            return True
+
+        return has_valid_scope(request, scope)
 
 
 class CSVView(APIView):
--- test/views/unit/common_tests.py
+++ test/views/unit/common_tests.py
@@ -1,4 +1,30 @@
-from common.views import CSVView
+import pytest
+
+from common.views import CSVView, BaseView
+
+
+def test_base_view_check_permissions_returns_true_if_auth_disabled(mocker):
+    assert BaseView().check_permissions(mocker.Mock()) is True
+
+
+@pytest.mark.usefixtures("enable_auth")
+def test_base_view_check_permissions_returns_true_if_no_scope_found(request_builder):
+    request = request_builder("get", "/some/endpoint")
+
+    assert BaseView().check_permissions(request) is True
+
+
+@pytest.mark.usefixtures("enable_auth")
+def test_base_view_returns_result_of_has_valid_scope(request_builder, mocker):
+    has_valid_scope = mocker.patch("common.views.has_valid_scope")
+    request = request_builder("get", "/some/endpoint")
+
+    view = BaseView()
+    view.scopes = {"get": "some:scope"}
+
+    response = view.check_permissions(request)
+    assert response == has_valid_scope.return_value
+    has_valid_scope.assert_called_once_with(request, "some:scope")
 
 
 def test_csv_view(mocker):
