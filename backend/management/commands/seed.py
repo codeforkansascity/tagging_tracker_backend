1b73654186d052d98fbd1d3954fe8a0a0d17fd1a
Adam Mertz <adammertz@gmail.com>
2019-02-09 01:19:01 -0600

    Created Contact record in 'seed' command

--- backend/management/commands/seed.py
+++ backend/management/commands/seed.py
@@ -3,14 +3,16 @@ from django.core.management import BaseCommand
 from django.utils import timezone
 from faker import Faker
 
-from backend.models import Address, Tag
+from backend.management.commands._helpers import init_contact_types
+from backend.models import Address, Tag, Contact, ContactType
 
 
 class Command(BaseCommand):
     help = "Seed database with fake data"
 
-
     def handle(self, *args, **options):
+        init_contact_types(self.stdout, self.style)
+
         fake = Faker()
 
         address = Address(
@@ -27,6 +29,16 @@ class Command(BaseCommand):
         )
         address.save()
         address.refresh_from_db()
+
+        contact = Contact(
+            address=address,
+            contact_type=ContactType.objects.get(slug=ContactType.Types.OWNER.value),
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            phone=fake.phone_number()
+        )
+        contact.save()
 
         tag = Tag(
             address=address,
