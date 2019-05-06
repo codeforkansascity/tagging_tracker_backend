ffad247cd8e43109d0aa3fa3a8fac40625ced878
Adam Mertz <adammertz@gmail.com>
2019-05-06 18:41:37 -0500

    Dropped travis role

--- tf/global/container-repo/main.tf
+++ tf/global/container-repo/main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "binfra" {
   location = "${var.location}"
-  name = "basicInfrastrucure"
+  name = "containerInfra"
 }
 
 
@@ -18,17 +18,4 @@ resource "azurerm_container_registry" "acr" {
   name = "wpR6r5k9hDLpK1VPEG6l"
   sku = "Basic"
   resource_group_name = "${azurerm_resource_group.binfra.name}"
-}
-
-resource "azurerm_user_assigned_identity" "travisci" {
-  location = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.binfra.name}"
-  name = "travis-ci"
-}
-
-
-resource "azurerm_role_assignment" "travisci" {
-  principal_id = "${azurerm_user_assigned_identity.travisci.principal_id}"
-  scope = "${azurerm_resource_group.binfra.id}"
-  role_definition_name = "acrpush"
 }
