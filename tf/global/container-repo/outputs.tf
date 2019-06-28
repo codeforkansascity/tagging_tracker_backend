1108d94337a1d87195115af3f47f1a9da8cef467
Adam Mertz <adammertz@gmail.com>
2019-06-27 19:30:34 -0500

    Actually return travis ci app id

--- tf/global/container-repo/outputs.tf
+++ tf/global/container-repo/outputs.tf
@@ -3,5 +3,5 @@ output "container_repo_url" {
 }
 
 output "travisci_app_id" {
-  value = "${azurerm_azuread_service_principal.travisci.id}"
+  value = "${azurerm_azuread_service_principal.travisci.application_id}"
 }
