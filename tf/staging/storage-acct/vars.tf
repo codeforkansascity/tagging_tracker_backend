077176131e39a0b9e84333e8fb234452e9f1f544
Adam Mertz <adammertz@gmail.com>
2019-07-01 19:07:55 -0500

    Storage account tf

---
+++ tf/staging/storage-acct/vars.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {
+  type = "string"
+}
+variable "client_id" {
+  type = "string"
+}
+variable "client_secret" {
+  type = "string"
+}
+variable "tenant_id" {
+  type = "string"
+}
+variable "location" {
+  default = "Central US"
+  type = "string"
+}
