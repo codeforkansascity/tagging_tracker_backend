2f34ba3cd5a0ac42ca287dd064afe42d537a79c9
Adam Mertz <adammertz@gmail.com>
2019-08-05 18:38:48 -0500

    strtobool needs actual strings

--- tagging_tracker/settings.py
+++ tagging_tracker/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = strtobool(os.environ.get("DEBUG"))
-DISABLE_AUTH = strtobool(os.getenv("DISABLE_AUTH"))
+DEBUG = strtobool(os.environ.get("DEBUG", str(False)))
+DISABLE_AUTH = strtobool(os.getenv("DISABLE_AUTH", str(False)))
 
 if DEBUG:
     SECRET_KEY = "mysecretkey"
