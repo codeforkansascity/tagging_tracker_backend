7e6fd785711da84a8b824f83d96706f90f91eedf
Adam Mertz <adammertz@gmail.com>
2019-09-23 17:44:45 -0500

    Removed duplicate imports

--- test_fixtures/__init__.py
+++ test_fixtures/__init__.py
@@ -5,10 +5,3 @@ from .model_builders import (
     contact_type_builder,
     contact_builder,
 )
-from .model_builders import (
-    property_type_builder,
-    address_builder,
-    tag_builder,
-    contact_type_builder,
-    contact_builder,
-)
